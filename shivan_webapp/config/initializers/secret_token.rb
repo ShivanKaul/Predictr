711e66a853f9685cf8d3123e123a1268ef5fd62b
Shivan <shivankaul.1993@gmail.com>
2013-12-21 22:18:34 -0700

    added shivan_webapp

---
+++ shivan_webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShivanWebapp::Application.config.secret_token = '1b0956b48b59679ee6d229f8163dc7d71c7a7c81e20a59fdd07b80b429b24834e1c8a59cb17de020d14e8618a88babc052c2a89409a7151f6e0158b8bb8566ec'
